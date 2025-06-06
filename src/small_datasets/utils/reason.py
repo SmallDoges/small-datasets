7cf16cec2d7dbb2ea4365b4373a48973b3ab0596
wubingheng111 <2245657493@qq.com>
2025-06-06 10:14:19 +0800

    fix pronunciation bug

--- src/small_datasets/utils/reason.py
+++ src/small_datasets/utils/reason.py
@@ -30,10 +30,10 @@ class Reasoner(curator.LLM):
         }
 
 
-def reason(dataset, base_url, model_name, temperture=0.0, max_tokens=8192, system_prompt_type="english", max_requests_per_minute=1_000, max_tokens_per_minute=1_000_000_000):
+def reason(dataset, base_url, model_name, temperature=0.0, max_tokens=8192, system_prompt_type="english", max_requests_per_minute=1_000, max_tokens_per_minute=1_000_000_000):
     reasoner = Reasoner(
         model_name=model_name,
-        generation_params={"temp": temperture, "max_tokens": max_tokens},
+        generation_params={"temp": temperature, "max_tokens": max_tokens},
         backend_params={"base_url": base_url, "max_requests_per_minute": max_requests_per_minute, "max_tokens_per_minute": max_tokens_per_minute, "max_retries": 1, "require_all_responses": False},
         system_prompt=REASONING_PROMPT[system_prompt_type],
     )
--- src/small_datasets/utils/translate.py
+++ src/small_datasets/utils/translate.py
@@ -3,7 +3,7 @@ from bespokelabs import curator
 from ..utils.prompt import TRANSLATION_PROMPT
 
 
-class Translater(curator.LLM):
+class Translator(curator.LLM):
     return_completions_object = True
 
     def __init__(self, model_name, generation_params, backend_params, system_prompt=None):
@@ -28,10 +28,10 @@ class Translater(curator.LLM):
         }
 
 
-def translate(dataset, base_url, model_name, temperture=0.0, max_tokens=8192, system_prompt_type="english", max_requests_per_minute=1_000, max_tokens_per_minute=1_000_000_000):
-    reasoner = Translater(
+def translate(dataset, base_url, model_name, temperature=0.0, max_tokens=8192, system_prompt_type="english", max_requests_per_minute=1_000, max_tokens_per_minute=1_000_000_000):
+    reasoner = Translator(
         model_name=model_name,
-        generation_params={"temp": temperture, "max_tokens": max_tokens},
+        generation_params={"temp": temperature, "max_tokens": max_tokens},
         backend_params={"base_url": base_url, "max_requests_per_minute": max_requests_per_minute, "max_tokens_per_minute": max_tokens_per_minute, "max_retries": 10, "require_all_responses": False},
         system_prompt=TRANSLATION_PROMPT[system_prompt_type],
     )
